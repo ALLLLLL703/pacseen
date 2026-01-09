5e3e9d680312e9ee7798634b33d8dce847e4f80e
ALLLLLL703 <vapev433@qq.com>
2026-01-09 11:50:15 +0800

    pacseen v1.0

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,41 @@
+use core::result;
+use std::error::Error;
+
 use color_eyre::eyre::{Ok, Result};
 use crossterm::event::{self, Event};
 
-use ratatui::{
-    DefaultTerminal, Frame,
-    crossterm::terminal,
-    prelude::Widget,
-    style::Stylize,
-    text::{Line, Text},
-    widgets::{Block, Paragraph},
-};
+use ratatui::DefaultTerminal;
 
+use crate::backend::load_repo_packages;
 use crate::objects::stat::App;
 use crate::ui::render;
 
 pub mod backend;
 pub mod objects;
+#[cfg(test)]
+pub mod test;
 pub mod ui;
-fn main() {
-    let mut state = App::default();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut state = App::new()?;
     color_eyre::install();
 
     let terminal = ratatui::init();
     let result = run(terminal, &mut state);
 
     ratatui::restore();
+    result::Result::Ok(())
 }
 
-fn run(mut terminal: DefaultTerminal, app: &mut App) -> Result<()> {
+pub fn run(mut terminal: DefaultTerminal, app: &mut App) -> Result<()> {
     loop {
         //Rendering
         terminal.draw(|frame| render(frame, app))?;
         //input handling
         if let Event::Key(key) = event::read()? {
-            match key.code {
-                event::KeyCode::Esc | event::KeyCode::Char('q') => {
-                    break;
-                }
-                _ => {}
+            app.handle_key(key);
+            if app.exit == true {
+                break;
             }
         }
     }
--- src/objects/stat.rs
+++ src/objects/stat.rs
@@ -1,5 +1,9 @@
-use std::alloc::Global;
-use std::error::Error;
+use std::{
+    error::Error,
+    fmt::{Display, Formatter},
+};
+
+use ratatui::widgets::ListState;
 
 use crate::backend::load_repo_packages;
 
@@ -40,27 +44,62 @@ pub enum ItemRepo {
     AUR,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Clone, Copy, Default)]
+pub struct InsertMode {
+    pub enabled: bool,
+    pub index: usize,
+}
+
+#[derive(Debug)]
 pub struct App {
-    items: Vec<Package>,
-    exit: bool,
-    filtered: Vec<Package>,
-    search: String,
-    selected: usize,
+    pub items: Vec<Package>,
+    pub exit: bool,
+    pub filtered: Vec<Package>,
+    pub search: String,
+    pub selected_pack: usize,
+    pub list_state: ListState,
+    pub selected_win: Window,
+    pub insert_mode: InsertMode,
+}
+
+#[derive(Debug, Clone)]
+pub enum Window {
+    Info,
+    Search,
+    List,
 }
 
 impl App {
     pub fn new() -> Result<Self, Box<dyn Error>> {
         let all_packages = load_repo_packages()?;
-
+        let mut list_state = ListState::default();
+        list_state.select(Some(0));
         let app = Self {
             filtered: all_packages.clone(),
             exit: false,
             search: String::new(),
-            selected: 0,
+            selected_pack: 0,
             items: all_packages,
+            list_state: list_state,
+            selected_win: Window::Search,
+            insert_mode: InsertMode {
+                enabled: false,
+                index: 0,
+            },
         };
         Ok(app)
+    }
+
+    pub fn update_filter(&mut self) {
+        self.filtered = self
+            .items
+            .iter()
+            .filter(|p| p.name.contains(&self.search))
+            .cloned()
+            .collect();
+        if self.selected_pack >= self.filtered.len() {
+            self.selected_pack = 0;
+        }
     }
 }
 
